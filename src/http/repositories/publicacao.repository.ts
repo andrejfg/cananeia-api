56f8d8233ec4ed60a9647b11129ce944dec9fd8c
André Felipe Guimarães <andrefg.dev@gmail.com>
2023-12-25 09:47:29 -0300

    imagem de perfil para participante e polo com gerenciamento de arquivo

---
+++ src/http/repositories/publicacao.repository.ts
@@ -0,0 +1,13 @@
+class publicacaoRepository {
+  async findAll() {}
+
+  async findById(id: string) {}
+
+  async add(data: any) {}
+
+  async removeById(id: string) {}
+
+  async updateById(id: string, data: any) {}
+}
+
+export default new publicacaoRepository()
