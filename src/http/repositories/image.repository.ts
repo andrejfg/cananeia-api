8b614b0c7e530917326b4932be4a4de15c2111a6
André Felipe Guimarães <andrefg.dev@gmail.com>
2024-01-09 10:01:53 -0300

    debug

--- src/http/repositories/image.repository.ts
+++ src/http/repositories/image.repository.ts
@@ -10,9 +10,13 @@ const blurhash = 'AGF5?xYk@-5c'
 
 class ImageRepository {
   async add({ image, proporcao = '1/1' }: AddImageDTO) {
+    console.log('Teste 1')
     const prefix = randomUUID()
+    console.log('Teste 2')
     const extension = extname(image.name)
+    console.log('Teste 3')
     const nome = prefix.concat(extension)
+    console.log('Teste 4')
     // const { data, info } = await sharp(await image.arrayBuffer())
     //   .ensureAlpha()
     //   .raw()
@@ -30,10 +34,12 @@ class ImageRepository {
       pathToPublic.concat('/' + nome),
       await image.arrayBuffer(),
     )
+    console.log('Teste 5')
     if (teste) {
       const newImage = await prisma.imagem.create({
         data: { hash: blurhash, proporcao, nome },
       })
+      console.log('Teste 6')
       return newImage
     }
   }
