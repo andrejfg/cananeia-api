ddad706bca3ce18e51b6ef1ab7fd55a81bbf37b0
André Felipe Guimarães <andrefg.dev@gmail.com>
2024-01-09 10:11:12 -0300

    tirando blurhash

--- src/http/repositories/image.repository.ts
+++ src/http/repositories/image.repository.ts
@@ -4,27 +4,25 @@ import { extname, resolve } from 'path'
 import { randomUUID } from 'crypto'
 import { unlinkSync } from 'fs'
 import { pathToPublic } from '../server'
-import { encode } from 'blurhash'
-import sharp from 'sharp'
-const blurhash = 'AGF5?xYk@-5c'
+const hash = 'AGF5?xYk@-5c'
 
 class ImageRepository {
   async add({ image, proporcao = '1/1' }: AddImageDTO) {
     const prefix = randomUUID()
     const extension = extname(image.name)
     const nome = prefix.concat(extension)
-    const { data, info } = await sharp(await image.arrayBuffer())
-      .ensureAlpha()
-      .raw()
-      .toBuffer({ resolveWithObject: true })
+    // const { data, info } = await sharp(await image.arrayBuffer())
+    //   .ensureAlpha()
+    //   .raw()
+    //   .toBuffer({ resolveWithObject: true })
 
-    const hash = encode(
-      new Uint8ClampedArray(data),
-      info.width,
-      info.height,
-      2,
-      2,
-    )
+    // const hash = encode(
+    //   new Uint8ClampedArray(data),
+    //   info.width,
+    //   info.height,
+    //   2,
+    //   2,
+    // )
 
     const teste = await Bun.write(
       pathToPublic.concat('/' + nome),
